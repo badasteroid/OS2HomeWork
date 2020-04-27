98971881f58f0fa74da5a40058e431750ba18d8a
desertornado <amenichelli1991@gmail.com>
2020-04-27 23:15:55 +0200

    On branch wip  Changes to be committed: 	modified:   main.c  Untracked files: 	a.txt 	t1

--- main.c
+++ main.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
-#include <limit.h>
-/*
-static void list_dir (const char * dir_name){
+#include <limits.h>
+#include <unistd.h>
 
-	DIR *d;
-	
-	d = opendir(dir_name);
-	
-	if (!d){
-	
-		fprintf(stderr, "cannot open dir '%s': %s\n", dir_name, strerror(errno));
-		exit(EXIT_FAILURE);	
-	
-	}
-
-	while(1){
-
-		struct dirent * entry;
-		const char * d_name;
-	
-		if(!entry)
-			break;
-		d_name = entry->d_name;	
-	
-	}
-
-}
-*/
 int copyFile(FILE *sourcefp, char *sourcename, int mask /*0 non executable, other  executable*/){
 	FILE * destfp = fopen(sourcename, "w");
 	int samefiles=0;
-	if(existsInDir(sourcename, mask)
+	char c;
+	/*if(existsInDir(sourcename, mask)){
 		strcat(sourcename,"1");
-		*sourcefp = fopen(sourcename, "r");
+		sourcefp = fopen(sourcename, "r");
+	}
 	else{
-		*sourcefp = fopen(sourcename,"r");
+		sourcefp = fopen(sourcename,"r");
 	}
 	int flag=0;
 	if(sourcefp){
@@ -50,17 +28,27 @@ int copyFile(FILE *sourcefp, char *sourcename, int mask /*0 non executable, othe
 		int mask=0777; //da rivedere
 		chmod(destfp, mask);
 	}
-	return flag;
+	return flag;*/
 }
 
-*char destFolderManagement(FILE *sourcefp, char *fname){
-	*sourcefp = open(
-	
+void isExecutable(FILE *sourcefp,const char *fname){
+	sourcefp = fopen(fname, "r");
+	struct stat *statfile;
+	char mode;
+	if(stat(fname, statfile)==0){
+		mode_t perm = statfile->st_mode;
+		mode = (perm & S_IXUSR) ? 'x' : '-';
+		printf("%s is %c\n", fname, mode);
+	}
+	printf("fuck");
 }
 
-int existsInDir(
+//int existsInDir(
  
 int main (int argc, char *argv){
 	
-
+	FILE *test;
+	char *tname ="/a.txt";
+	isExecutable(test, tname);
+	return 0;
 }
